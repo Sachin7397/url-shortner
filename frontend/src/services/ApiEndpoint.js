b145c5b29baf7b6db4b1def7228d29ef9be5015c
Sachin <110655633+Sachin7397@users.noreply.github.com>
2025-01-02 05:00:11 +0530

    Update ApiEndpoint.js

--- frontend/src/services/ApiEndpoint.js
+++ frontend/src/services/ApiEndpoint.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8001/url",
+  baseURL: "https://url-shortner-backend-u106.onrender.com/url",
   headers: {
     "Content-Type": "application/json",
   },
